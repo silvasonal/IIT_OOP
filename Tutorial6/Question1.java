cee13c0ce5777041e27bf335c70760e7c03767d1
silvasonal <88979538+silvasonal@users.noreply.github.com>
2022-11-23 10:19:36 +0530

    Update Question1.java

--- Tutorial6/Question1.java
+++ Tutorial6/Question1.java
@@ -1,1 +1,31 @@
+public class Question1 {
+    
+
+    public static void loadArray(int[] list) {
+        for (int i = 1; i < list.length; i++)
+        {
+            list[i] = list[i] + list[i - 1];
+        }
+        
+        for(int i=0;i<list.length;i++){
+            System.out.println(list[i]);
+        }
+    }
+    
+    public static void main(String[] args) {
+        
+        int[] numbers = {3, 6};
+
+        loadArray(numbers);
+
+    }
+
+}
+
+//output
+//a) -
+//b) 3,9
+//c) 4,10,18
+//d) 1,3,6,10
+//e) 8,12,14,14,18
 
